efec1096f9f3c71776bec55acbd427b9424b820c
benjaminpettus <benjaminpettus@gmail.com>
2016-02-27 12:34:28 -1000

    setting up database

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
+var Sequelize = require('sequelize');
+var db = require('./models');
 
 var app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//main page
+app.get('/api', function (req, res) {
+ res.send('string of something');
+});
+
+//saving a new card to database
+app.post('/api', function (req, res) {
+ 
+  res.json(req.body);
+});
+
+
 
 
 
