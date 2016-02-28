4c477432738ec59eedefa60ce51f8d2138e70cf6
benjaminpettus <benjaminpettus@gmail.com>
2016-02-27 19:23:53 -1000

    getting db data to display in cards

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,9 +3,12 @@ var myApp = angular.module('myApp');
 myApp.controller('CardController', [
   '$scope',
   'CardService',
+
   function ($scope, CardService) {
-    $scope.card = "card1";
-    $scope.CardService = CardService;
+    CardService.getCards()
+      .then(function (response) {
+        $scope.cards = response.data;
+      });
   }
     
 ]);
--- public/js/services.js
+++ public/js/services.js
@@ -1,26 +1,24 @@
 var myApp = angular.module('myApp');
 
-myApp.service('CardService', CardService);
 
-function CardService() {
-  var cards = this.cards = [
-  {title: routes, priority: low, created_by: ben, assigned_to: me }
-  ];
-}
 
-this.addCard = function(title, priority, created_by, assigned_to){
-  var newCard = {
-    title: title,
-    priority: priority,
-    created_by: created_by,
-    assigned_to: assigned_to
-  };  
-  cards.push(newCard);
-};
+myApp.service('CardService', ['$http', function ($http) {
 
-this.getCards = function () {
-  if (index < 0 || index > cards.length) {
-    return null;
-  }
-  return cards[index];
-};
+  this.addCard = function(title, priority, created_by, assigned_to){
+    var newCard = {
+      title: title,
+      priority: priority,
+      created_by: created_by,
+      assigned_to: assigned_to
+    };  
+    cards.push(newCard);
+  };
+
+  this.getCards = function () {
+    return $http({
+      method:'GET',
+      url: '/api'
+    });
+    
+  };
+}]);
